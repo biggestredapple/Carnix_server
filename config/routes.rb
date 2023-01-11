aad5466e741d538b88003ab2be8d21fb7a361212
apple <dan.hassan0910@gmail.com>
2023-01-11 11:56:39 +0900

    category & order

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post "/upload", to: "scales#upload"
-  get "/scales", to: "scales#index"
+  get "/scales/:category", to: "scales#index"
+  get "/categories", to: "scales#categories"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
